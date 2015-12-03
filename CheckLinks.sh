85410805043459848d71102f147dee05f264063b
heitorbarbieri <barbieri@paho.org>
2015-12-03 15:43:16 -0200

    fixing some bugs

--- CheckLinks.sh
+++ CheckLinks.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CHECK_LINKS_HOME=/home/users/heitor.barbieri/sbt-projects/CheckLinks
+
 if [ "$#" -lt 4 ]; then
   echo "usage: <inFile> <outGoodFile> <outBrokenFile> <outMongoBrokenFile> [<encoding>]"
   exit 1
@@ -21,9 +23,9 @@ echo
 echo "-----------------------------------------------------------------------"
 echo "CheckLinksApplication - checking $lines links"
 #sbt "run-main org.bireme.cl.CheckLinksApplication $1 $2 $3 $5"
-java -cp target/scala-2.11/CheckLinks-assembly-1.0.jar org.bireme.cl.CheckLinksApplication $1 $2 $3 $5
+java -cp $CHECK_LINKS_HOME/target/scala-2.11/CheckLinks-assembly-1.0.jar org.bireme.cl.CheckLinksApplication $1 $2 $3 $5
 
 echo
 echo "Checking if broken urls were broken some time ago ..."
 #sbt "run-main org.bireme.murl.MongoUrlApp mongodb.bireme.br $3 $4 $5"
-java -cp target/scala-2.11/CheckLinks-assembly-1.0.jar org.bireme.murl.MongoUrlApp mongodb.bireme.br $3 $4 $5
+java -cp $CHECK_LINKS_HOME/target/scala-2.11/CheckLinks-assembly-1.0.jar org.bireme.murl.MongoUrlApp mongodb.bireme.br $3 $4 $5
