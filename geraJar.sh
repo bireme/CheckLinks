317fc2428e640076dbf4afa9212423b0886a4fe6
heitorbarbieri <barbieri@paho.org>
2016-04-15 12:40:20 -0300

    wait parameter added

--- geraJar.sh
+++ geraJar.sh
@@ -1,3 +1,3 @@
 sbt clean assembly
 
-cp /home/heitor/sbt-projects/CheckLinks/target/scala-2.11/CheckLinks-assembly-1.0.jar .
+cp /home/heitor/sbt-projects/CheckLinks/target/scala-2.11/CheckLinks-assembly-1.0.jar ./CheckLinks.jar
